94d76726281a6e63acec78238e5dc8914f9d87ba
Kristofor Maynard <k.maynard@unh.edu>
2015-04-15 20:57:13 -0400

    update docs to 0.80.0

--- ex_reader-1.py
+++
@@ -1,8 +0,0 @@
-import viscid
-from viscid.plot import mpl
-
-f3d = viscid.load_file(_viscid_root + '/../sample/sample.3df.xdmf')
-# notice y=0.0, this is different from y=0; y=0 is the 0th index in
-# y, which is this case will be y=-50.0
-pp = f3d["pp"]["y=0.0"]
-mpl.plot(pp, plot_opts="log")
--- ex_slicing-1.py
+++
@@ -1,9 +0,0 @@
-import viscid
-from viscid.plot import mpl
-
-f3d = viscid.load_file(_viscid_root + '/../sample/sample.3df.xdmf')
-
-# notice how slices by index appear as integers, and slices by location
-# are done with floats... this means "y=0" is not the same as "y=0.0"
-pp = f3d["pp"]["x=50:-30,y=0.0,z=-10.0:10.0"]
-mpl.plot(pp, style="contourf", levels=50, plot_opts="log,earth")
